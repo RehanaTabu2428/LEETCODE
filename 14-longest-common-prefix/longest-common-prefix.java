5ad0334e9a28b1eb177dd2708666312d4f83052a
Rehana Tabassum <rehanatabassumjosephite2428@gmail.com>
2026-05-05 20:04:43 +0530

    Time: 4 ms (13.29%) | Memory: 43.5 MB (17.40%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0) return "";
+        String result="";
+        for(int i=0;i<strs[0].length();i++){
+            char curr=strs[0].charAt(i);
+            for(int j=1;j<strs.length;j++){
+                if(i>=strs[j].length()||strs[j].charAt(i)!=curr){
+                    return result;
+                }
+            }
+            result+=curr;
+        }
+        return result;
+    }
+}

4b19ce34610e5ec605371cfca7041336c0956afd
Rehana Tabassum <rehanatabassumjosephite2428@gmail.com>
2026-09-08 12:11:08 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (28.56%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,8 +1,29 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target) return true;
-        }
-        return false;
+        int l=0;
+        int r=nums.length-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+
+            if(nums[mid]==target) return true;
+
+            if(nums[l]==nums[mid] && nums[mid]==nums[r]){
+                l++;
+                r--;
+            }
+            else if(nums[l]<=nums[mid]){
+             if(target>=nums[l] && target<nums[mid]){
+                  r=mid-1;
+            }
+            else l=mid+1;
+         }
+         else {
+            if(target>nums[mid] && target<=nums[r]){
+                l=mid+1;
+            }
+            else r=mid-1;
+         }
     }
+    return false;
+}
 }

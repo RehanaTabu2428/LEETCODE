22796fd63638411271b0cee1b2c297484edaf371
Rehana Tabassum <rehanatabassumjosephite2428@gmail.com>
2026-09-10 10:05:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (97.65%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -4,14 +4,12 @@ class Solution {
         int cols=matrix[0].length;
         int l=0;
         int r=rows*cols-1;
-        int ans=0;
         while(l<=r){
             int mid=l+(r-l)/2;
             int m=mid/cols;
             int n=mid%cols;
             if(matrix[m][n]==target) return true;
             if(matrix[m][n]<=target){
-                ans=mid;
                 l=mid+1;
             } 
             else r=mid-1;

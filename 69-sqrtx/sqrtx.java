00f900db1bc7f1e33561876bebefcffbbf5c804f
Rehana Tabassum <rehanatabassumjosephite2428@gmail.com>
2026-09-13 16:24:49 +0530

    Time: 1 ms (99.13%) | Memory: 42.8 MB (23.72%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,18 +1,16 @@
 public class Solution {
     public int mySqrt(int x) {
+        if(x==0||x==1) return x;
         int l = 0, r = x;
         int res = 0;
 
         while (l <= r) {
             int m = l + (r - l) / 2;
-            if ((long) m * m > x) {
-                r = m - 1;
-            } else if ((long) m * m < x) {
-                l = m + 1;
-                res = m;
-            } else {
-                return m;
+            if(m<=x/m){
+                res=m;
+                l=m+1;
             }
+            else r=m-1;
         }
 
         return res;

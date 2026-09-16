cd5df3692ef75a11688d1b1593374367477733f0
Rehana Tabassum <rehanatabassumjosephite2428@gmail.com>
2026-09-16 09:12:16 +0530

    Time: 1 ms (100.00%) | Memory: 47.8 MB (76.86%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor=0;
+        for(int i:nums){
+            xor=xor^i;
+        }
+        int bit=xor & -xor;
+        int num1=0;
+        int num2=0;
+
+        for(int num:nums){
+            if((num & bit) ==0) num1=num1 ^ num;
+            else num2=num2 ^ num;
+        }
+        return new int[]{num1,num2};
+    }
+}

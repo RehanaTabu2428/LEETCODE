c09fd557f8978334039029420789e0a68101b08f
Rehana Tabassum <rehanatabassumjosephite2428@gmail.com>
2026-09-16 08:20:34 +0530

    Time: 2 ms (23.52%) | Memory: 43.4 MB (97.42%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -3,6 +3,14 @@ class Solution {
         for(int i=0;i<n;i++){
             nums1[i+m]=nums2[i];
         }
-        Arrays.sort(nums1);
+        for(int i=0;i<nums1.length-1;i++){
+            for(int j=0;j<nums1.length-1-i;j++){
+                if(nums1[j]>nums1[j+1]){
+                    int temp=nums1[j];
+                    nums1[j]=nums1[j+1];
+                    nums1[j+1]=temp;
+                }
+            }
+        }
     }
 }
